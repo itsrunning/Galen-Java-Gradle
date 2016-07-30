76db03794e12a5940d22acd18176e972dd6faed9
Jaya Chakravarthi Kuppu <jayachk@thoughtworks.com>
2016-07-30 11:09:19 +0530

    Jaya/Sneha/Gowri/Ajay:first commit

---
+++ src/test/java/galen/components/GalenTestBase.java
@@ -0,0 +1,45 @@
+package galen.components;
+
+import com.galenframework.testng.GalenTestNgTestBase;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.DataProvider;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+public abstract class GalenTestBase extends GalenTestNgTestBase {
+
+    private static final String ENV_URL = "http://testapp.galenframework.com";
+
+    @Override
+    public WebDriver createDriver(Object[] args) {
+        WebDriver driver = new FirefoxDriver();
+        if (args.length > 0) {
+            if (args[0] != null && args[0] instanceof TestDevice) {
+                TestDevice device = (TestDevice)args[0];
+                if (device.getScreenSize() != null) {
+                    driver.manage().window().setSize(device.getScreenSize());
+                }
+            }
+        }
+        return driver;
+    }
+
+    public void load(String uri) {
+        getDriver().get(ENV_URL + uri);
+    }
+
+    @DataProvider(name = "devices")
+    public Object [][] devices () {
+        return new Object[][] {
+                {new TestDevice("mobile", new Dimension(450, 800), asList("mobile"))},
+                {new TestDevice("tablet", new Dimension(750, 800), asList("tablet"))},
+                {new TestDevice("desktop", new Dimension(1024, 800), asList("desktop"))}
+        };
+    }
+
+
+}
---
+++ src/test/java/galen/tests/WelcomePageTest.java
@@ -0,0 +1,26 @@
+package galen.tests;
+
+import galen.components.GalenTestBase;
+import galen.components.TestDevice;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+
+public class WelcomePageTest extends GalenTestBase {
+
+    @Test(dataProvider = "devices")
+    public void welcomePage_shouldLookGood_onDevice(TestDevice device) throws IOException {
+        load("/");
+        checkLayout("/specs/welcomePage.spec", device.getTags());
+    }
+
+    @Test(dataProvider = "devices")
+    public void loginPage_shouldLookGood_onDevice(TestDevice device) throws IOException {
+        load("/");
+        getDriver().findElement(By.xpath("//button[.='Login']")).click();
+        checkLayout("/specs/loginPage.spec", device.getTags());
+    }
+
+}
